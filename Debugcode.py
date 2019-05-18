e6295b9cf55a7a9d9cf3c744d6d0ab4a54d909fa
samlianGitHub <50382450+samlianGitHub@users.noreply.github.com>
2019-05-18 15:36:49 -0700

    Update Debugcode.py

--- Debugcode.py
+++ Debugcode.py
@@ -1,11 +1,14 @@
 def lone_sum(a, b, c):
-    if a >= b:
+    if a == b and a == c and b == c:
+        return 0
+    elif a >= b:
         return c
-    elif a == c:
-        return b
     elif b == c:
         return a
-    elif a == b and a == c and b == c:
-        return 0
+    elif a == c:
+     return c
     else:
         return a+b+c
+print(lone_sum(1,1,2)) # Add the number to print and I indent some code it work 
+#I Debuge you code and I fixing some line 
+# Tank to you Powerful1
