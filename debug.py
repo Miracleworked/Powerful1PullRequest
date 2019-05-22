49c87f5b329e2db46af20554c91fe822e58d05a3
samlianGitHub <50382450+samlianGitHub@users.noreply.github.com>
2019-05-21 19:33:51 -0700

    Update debug.py

--- debug.py
+++ debug.py
@@ -1,15 +1,17 @@
 #Broken code
 def lone_sum(a, b, c):
-    if a >= b:
-        return c
-    elif a == c:
-        return b
-    elif b == c:
-        return a
     elif a == b and a == c and b == c:
         return 0
+    if a >= b:
+        return c
+    elif b == c:
+        return a
+    elif a == c:
+        return c
     else:
         return a+b+c
+print(lone_sum(1,2,3))
+
 
 
 
